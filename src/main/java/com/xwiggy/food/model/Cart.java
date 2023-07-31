a8558aa63186734e96eb7f3ba9d38b7fc53a669b
Sukanya <sukanyasamminabhavi@gmail.com>
2023-07-31 12:36:55 +0530

    Initial commit for the project

---
+++ src/main/java/com/xwiggy/food/model/Cart.java
@@ -0,0 +1,134 @@
+package com.xwiggy.food.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Cart {
+
+    @Id
+    private int quantity1;
+    private int quantity2;
+    private int quantity3;
+    private int quantity4;
+    private int quantity5;
+    private int quantity6;
+    private int quantity7;
+	private int quantity8;
+    private int quantity9;
+    private int quantity10;
+    
+
+    public Cart(){}
+
+    public Cart(int quantity1, int quantity2, int quantity3, int quantity4, int quantity5, int quantity6, int quantity7,
+    		int quantity8, int quantity9, int quantity10) {
+        this.quantity1 = quantity1;
+        this.quantity2 = quantity2;
+        this.quantity3 = quantity3;
+        this.quantity4 = quantity4;
+        this.quantity5 = quantity5;
+        this.quantity6 = quantity6;
+        this.quantity2 = quantity7;
+        this.quantity3 = quantity8;
+        this.quantity4 = quantity9;
+        this.quantity5 = quantity10;
+        
+    }
+
+    public int getQuantity1() {
+        return quantity1;
+    }
+
+    public void setQuantity1(int quantity1) {
+        this.quantity1 = quantity1;
+    }
+
+    public int getQuantity2() {
+        return quantity2;
+    }
+
+    public void setQuantity2(int quantity2) {
+        this.quantity2 = quantity2;
+    }
+
+    public int getQuantity3() {
+        return quantity3;
+    }
+
+    public void setQuantity3(int quantity3) {
+        this.quantity3 = quantity3;
+    }
+
+    public int getQuantity4() {
+        return quantity4;
+    }
+
+    public void setQuantity4(int quantity4) {
+        this.quantity4 = quantity4;
+    }
+
+    public int getQuantity5() {
+        return quantity5;
+    }
+
+    public void setQuantity5(int quantity5) {
+        this.quantity5 = quantity5;
+    }
+
+    public int getQuantity6() {
+        return quantity6;
+    }
+
+    public void setQuantity6(int quantity6) {
+        this.quantity6 = quantity6;
+    }
+    
+    public int getQuantity7() {
+		return quantity7;
+	}
+
+	public void setQuantity7(int quantity7) {
+		this.quantity7 = quantity7;
+	}
+
+	public int getQuantity8() {
+		return quantity8;
+	}
+
+	public void setQuantity8(int quantity8) {
+		this.quantity8 = quantity8;
+	}
+
+	public int getQuantity9() {
+		return quantity9;
+	}
+
+	public void setQuantity9(int quantity9) {
+		this.quantity9 = quantity9;
+	}
+
+	public int getQuantity10() {
+		return quantity10;
+	}
+
+	public void setQuantity10(int quantity10) {
+		this.quantity10 = quantity10;
+	}
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "quantity1=" + quantity1 +
+                ", quantity2=" + quantity2 +
+                ", quantity3=" + quantity3 +
+                ", quantity4=" + quantity4 +
+                ", quantity5=" + quantity5 +
+                ", quantity6=" + quantity6 +
+                ", quantity3=" + quantity7 +
+                ", quantity4=" + quantity8 +
+                ", quantity5=" + quantity9 +
+                ", quantity6=" + quantity10 +
+                '}';
+    }
+}
